58d61c926f4ea42d7eae09e37b62b8fe62aa5f45
Siddhant Deshmukh <91955718+Siddhant2601@users.noreply.github.com>
2022-06-11 09:28:14 -0400

    Update Comparing_Numbers.sh

--- Hackerrank_solving/Comparing_Numbers.sh
+++ Hackerrank_solving/Comparing_Numbers.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# Taking user input 
 read x
 read y
 
+# Checking conditions
+
+# Less than condition
 if [ $x -lt $y ]; then
     echo "X is less than Y"
+
+# Greater than condition
 elif [ $x -gt $y ]; then
     echo "X is greater than Y"
+
+# Equal else condition
 else
     echo "X is equal to Y"
 fi
