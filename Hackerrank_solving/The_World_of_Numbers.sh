228a588438805dc6a348da6257cd091e511e2396
Siddhant Deshmukh <91955718+Siddhant2601@users.noreply.github.com>
2022-06-12 08:09:44 -0500

    Update The_World_of_Numbers.sh

--- Hackerrank_solving/The_World_of_Numbers.sh
+++ Hackerrank_solving/The_World_of_Numbers.sh
@@ -1,10 +1,19 @@
+# Taking input
 read x
 read y
+
+# sum
 Sum=$((x+y))  
 echo "$Sum"  
+
+# difference
 Diff=$((x-y))  
 echo "$Diff"
+
+# product
 prod=$((x*y))  
 echo "$prod"
+
+# division
 div=$((x/y))  
 echo "$div"
